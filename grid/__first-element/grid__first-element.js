288e5a9d677400d151d0faccd4d08a7cc0e4b599
DESKTOP-KC00KL8\Илья <ilarm8q@gmail.com>
2023-04-11 13:38:10 +0300

    Исправлено отображение блока "зарезервивовано" для мобильной и декстоп версии

--- grid/__first-element/grid__first-element.js
+++ grid/__first-element/grid__first-element.js
@@ -1,10 +1,19 @@
 function clickFirstCardButton() {
-  let discountBlock = document.querySelector(".grid__first-element > .room-card__discount-block");
-  let reserveBlock = document.querySelector(".grid__first-element > .room-card__bottom-reserve-container");
-  let gradient = document.querySelector(".grid__first-element > .room-card__reserve-block-background-gradient");
-  if (discountBlock && reserveBlock && gradient) {
-    discountBlock.style.display = "none";
-    reserveBlock.style.display = "flex";
+  let gradient = document.querySelector(
+    ".grid__first-element > .room-card__reserve-block-background-gradient"
+  );
+  let reserveBlockPaddingBlock = document.querySelector(
+    ".grid__first-element > .room-card__bottom-container > .room-card__bottom-block > .room-card__bottom-reserve-container-padding"
+  );
+  let bottomBlock = document.querySelector(
+    ".grid__first-element > .room-card__bottom-reserve-container"
+  );
+  console.log(gradient);
+  console.log(reserveBlockPaddingBlock);
+  console.log(bottomBlock);
+  if (gradient && bottomBlock && reserveBlockPaddingBlock) {
     gradient.style.display = "block";
+    bottomBlock.style.display = "flex";
+    reserveBlockPaddingBlock.style.display = "block";
   }
 }
--- grid/__third-element/grid__third-element.js
+++ grid/__third-element/grid__third-element.js
@@ -1,9 +1,11 @@
 function clickThirdCardButton() {
-  let discountBlock = document.querySelector(".grid__third-element > .room-card__discount-block");
-  let reserveBlock = document.querySelector(".grid__third-element > .room-card__bottom-reserve-container");
-  let gradient = document.querySelector(".grid__third-element > .room-card__reserve-block-background-gradient");
-  if (discountBlock && reserveBlock && gradient) {
-    discountBlock.style.display = "none";
+  let reserveBlock = document.querySelector(
+    ".grid__third-element > .room-card__bottom-reserve-container"
+  );
+  let gradient = document.querySelector(
+    ".grid__third-element > .room-card__reserve-block-background-gradient"
+  );
+  if (reserveBlock && gradient) {
     reserveBlock.style.display = "flex";
     gradient.style.display = "block";
   }
