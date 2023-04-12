6c1a34bb3a25a2ba5eb08e0b9548d043bd1957a4
DESKTOP-KC00KL8\Илья <ilarm8q@gmail.com>
2023-04-12 12:51:43 +0300

    Состоятние резервности появляется после выхода курсора

--- grid/__first-element/grid__first-element.js
+++ grid/__first-element/grid__first-element.js
@@ -1,3 +1,8 @@
+let firstCardState = {
+  'isReserved' : false,
+  'isClickReserveButton' : false,
+}
+
 function reserveFirstRoom() {
   let gradient = document.querySelector(
     ".grid__first-element > .room-card__reserve-block-background-gradient"
@@ -12,5 +17,35 @@ function reserveFirstRoom() {
     gradient.style.display = "block";
     bottomBlock.style.display = "flex";
     reserveBlockPaddingBlock.style.display = "block";
+    firstCardState['isReserved'] = true;
   }
+}
+
+function deReserveFirstRoom() {
+  let gradient = document.querySelector(
+    ".grid__first-element > .room-card__reserve-block-background-gradient"
+  );
+  let reserveBlockPaddingBlock = document.querySelector(
+    ".grid__first-element > .room-card__bottom-container > .room-card__bottom-block > .room-card__bottom-reserve-container-padding"
+  );
+  let bottomBlock = document.querySelector(
+    ".grid__first-element > .room-card__bottom-reserve-container"
+  );
+  if (gradient && bottomBlock && reserveBlockPaddingBlock) {
+    gradient.style.display = "none";
+    bottomBlock.style.display = "none";
+    reserveBlockPaddingBlock.style.display = "none";
+    firstCardState['isReserved'] = false;
+    firstCardState['isClickReserveButton'] = false;
+  }
+}
+
+function createObserver() {
+  firstCardState['isClickReserveButton'] = true;
+  document.querySelector(".grid__first-element")
+    .addEventListener("mouseout", function() {
+      if (!firstCardState['isReserved'] && firstCardState['isClickReserveButton']) {
+        reserveFirstRoom();
+      }
+    });
 }
